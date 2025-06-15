057318cbdd510fb2a06c57720013d6b0f936179e
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2025-06-15 19:57:58 -0300

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,25 +8,36 @@ public class Main {
         InsertionSort insertionSort = new InsertionSort();
         HeapSort heapSort = new HeapSort();
 
-        File pasta = new File("C:\\Users\\12922114821\\IdeaProjects\\Benchmark_algoritmos_ordenacao\\src\\dados");
-        File[] arquivos = pasta.listFiles();
+        String[] pastas = {
+                "aleatoria_10.txt",
+                "aleatoria_1000.txt",
+                "aleatoria_10000.txt",
+                "aleatoria_100000.txt",
+                "aleatoria_1000000.txt",
+                "ordenada_10.txt",
+                "ordenada_1000.txt",
+                "ordenada_10000.txt",
+                "ordenada_100000.txt",
+                "ordenada_1000000.txt",
+                "reversa_10.txt",
+                "reversa_1000.txt",
+                "reversa_10000.txt",
+                "reversa_100000.txt",
+                "reversa_1000000.txt",
+        };
 
         System.out.println("-------------- Tempo de execução em nanosegundos --------------");
         System.out.println();
 
-        if (arquivos != null) {
-            for (File arquivo : arquivos) {
-                CarregaDados carregaDados = new CarregaDados("dados" + "\\" + arquivo.getName());
-                int[] dadosOriginais = carregaDados.getValores();
+        for (String pasta : pastas) {
+            CarregaDados carregaDados = new CarregaDados(Paths.get("dados", pasta).toString());
+            int[] dadosOriginais = carregaDados.getValores();
 
-                System.out.println("Nome do arquivo: " + arquivo.getName());
-                System.out.printf("BubbleSort: %.6f \n", bubbleSort.testaAlgoritmo(dadosOriginais.clone()));
-                System.out.printf("InsertionSort: %.6f \n", insertionSort.testaAlgoritmo(dadosOriginais.clone()));
-                System.out.printf("HeapSort: %.6f \n", heapSort.testaAlgoritmo(dadosOriginais.clone()));
-                System.out.println();
-            }
-        } else {
-            System.out.println("A pasta está vazia ou não pode ser lida.");
+            System.out.println("Nome do arquivo: " + pasta);
+            System.out.printf("BubbleSort: %.6f \n", bubbleSort.testaAlgoritmo(dadosOriginais.clone()));
+            System.out.printf("InsertionSort: %.6f \n", insertionSort.testaAlgoritmo(dadosOriginais.clone()));
+            System.out.printf("HeapSort: %.6f \n", heapSort.testaAlgoritmo(dadosOriginais.clone()));
+            System.out.println();
         }
     }
 }
